9fa16730e5f797f50db5212c277c6e28e0bd2520
Dudisdu99 <131018122+Dudisdud99@users.noreply.github.com>
2023-11-14 22:34:53 +0000

    arrumando as logos

--- 001-ATIVIDADE- Menu hamburguer/assets/main.js
+++ 001-ATIVIDADE- Menu hamburguer/assets/main.js
@@ -14,51 +14,58 @@ const btDialog = document.querySelector(".bt-dialog")
 const btModal = document.querySelector(".bt-modal")
 const dialog = document.querySelector("dialog")
 
-window.onload = function() {
+window.onload = function () {
   atualizarCarrossel();
 };
 
 
-  
-  // Crie um array com os caminhos das imagens
-  const imagens = [
-    'assets/img/deathNote.webp',
-    'assets/img/dr-stone.jpg',
-    'assets/img/naruto.webp',
-  ];
-  
-  const imgFundo = document.querySelector('.imgFundo');
-  const degrade2 = document.querySelector('.degrade2');
-  const carrocel = document.querySelector('.carrocel');
 
-  var imgFundoWidth = imgFundo.clientWidth;
-  degrade2.style.width = `${imgFundoWidth}px`;
-  
-  // Variável para rastrear a posição atual no carrossel
-  let posicaoAtual = 0;
-  
-  function atualizarCarrossel() {
-    // Verifica se a posição atual é válida
-    if (posicaoAtual >= 0 && posicaoAtual < imagens.length) {
-      imgFundo.onload = function () {
-        // Use the 'load' event to ensure the image has been loaded
-        imgFundoWidth = imgFundo.clientWidth;
-        degrade2.style.width = `${imgFundoWidth}px`;
-      };
-  
-      imgFundo.src = imagens[posicaoAtual];
-    }
+// Crie um array com os caminhos das imagens
+const imagens = [
+  'assets/img/deathNote.webp',
+  'assets/img/dr-stone.jpg',
+  'assets/img/naruto.webp',
+];
+
+const logos = [
+  'assets/img/deathNote-logo.png',
+  'assets/img/Dr._Stone_logo.webp',
+  'assets/img/naruto-logo.png',
+];
+
+const imgFundo = document.querySelector('.imgFundo');
+const degrade2 = document.querySelector('.degrade2');
+const carrocel = document.querySelector('.carrocel');
+
+var imgFundoWidth = imgFundo.clientWidth;
+degrade2.style.width = `${imgFundoWidth}px`;
+
+// Variável para rastrear a posição atual no carrossel
+let posicaoAtual = 0;
+
+function atualizarCarrossel() {
+  // Verifica se a posição atual é válida
+  if (posicaoAtual >= 0 && posicaoAtual < imagens.length) {
+    imgFundo.onload = function () {
+      // Use the 'load' event to ensure the image has been loaded
+      imgFundoWidth = imgFundo.clientWidth;
+      degrade2.style.width = `${imgFundoWidth}px`;
+    };
+
+    imgFundo.src = imagens[posicaoAtual];
+    animeLogo.src = logos[posicaoAtual];
   }
-  
-  function proximaImagem() {
-    posicaoAtual++;
-    if (posicaoAtual >= imagens.length) {
-      posicaoAtual = 0; // Volta para a primeira imagem se atingir o final
-    }
-    atualizarCarrossel();
+}
+
+function proximaImagem() {
+  posicaoAtual++;
+  if (posicaoAtual >= imagens.length) {
+    posicaoAtual = 0; // Volta para a primeira imagem se atingir o final
   }
-  
-  // Configura um intervalo para trocar automaticamente as imagens a cada 5 segundos
-  setInterval(proximaImagem, 4000);
-  
+  atualizarCarrossel();
+}
+
+// Configura um intervalo para trocar automaticamente as imagens a cada 5 segundos
+setInterval(proximaImagem, 4000);
+
 // carrocel.addEventListener('click', proximaImagem);
