846122eb4ba8b3adce5dbd8de9aa0d3f6a5f05c0
Dudisdu99 <131018122+Dudisdud99@users.noreply.github.com>
2023-11-15 15:06:43 +0000

    fazendo os posters dos animes

--- 001-ATIVIDADE- Menu hamburguer/assets/main.js
+++ 001-ATIVIDADE- Menu hamburguer/assets/main.js
@@ -14,58 +14,87 @@ const btDialog = document.querySelector(".bt-dialog")
 const btModal = document.querySelector(".bt-modal")
 const dialog = document.querySelector("dialog")
 
-window.onload = function () {
-  atualizarCarrossel();
-};
 
-
-
-// Crie um array com os caminhos das imagens
-const imagens = [
-  'assets/img/deathNote.webp',
-  'assets/img/dr-stone.jpg',
-  'assets/img/naruto.webp',
-];
-
-const logos = [
-  'assets/img/deathNote-logo.png',
-  'assets/img/Dr._Stone_logo.webp',
-  'assets/img/naruto-logo.png',
-];
+// --------------------------------------------
 
 const imgFundo = document.querySelector('.imgFundo');
 const degrade2 = document.querySelector('.degrade2');
-const carrocel = document.querySelector('.carrocel');
+const imgLogo = document.querySelector('.imgLogo>img');
+const sinopse = document.querySelector('.carrocel p');
 
-var imgFundoWidth = imgFundo.clientWidth;
-degrade2.style.width = `${imgFundoWidth}px`;
 
-// Variável para rastrear a posição atual no carrossel
-let posicaoAtual = 0;
+async function main() {
 
-function atualizarCarrossel() {
-  // Verifica se a posição atual é válida
-  if (posicaoAtual >= 0 && posicaoAtual < imagens.length) {
-    imgFundo.onload = function () {
-      // Use the 'load' event to ensure the image has been loaded
-      imgFundoWidth = imgFundo.clientWidth;
-      degrade2.style.width = `${imgFundoWidth}px`;
-    };
+  const request = await fetch("assets/carrocel.json")
+  const carrocel = await request.json()
 
-    imgFundo.src = imagens[posicaoAtual];
-    animeLogo.src = logos[posicaoAtual];
+  // Crie um array com os caminhos das imagens
+  const imagens = [
+    'assets/img/deathNote.webp',
+    'assets/img/dr-stone.jpg',
+    'assets/img/naruto.webp',
+  ];
+
+  const logos = [
+    'assets/img/deathNote-logo.png',
+    'assets/img/Dr._Stone_logo.webp',
+    'assets/img/naruto-logo.png',
+  ];
+
+
+  var imgFundoWidth = imgFundo.clientWidth;
+  degrade2.style.width = `${imgFundoWidth}px`;
+
+  // Variável para rastrear a posição atual no carrossel
+  let posicaoAtual = 0;
+
+  function atualizarCarrossel() {
+    const transicao = document.querySelector('.transicao');
+    transicao.classList.add('darken');
+
+    if (posicaoAtual >= 0 && posicaoAtual < carrocel.length) {
+      imgFundo.onload = function () {
+        imgFundoWidth = imgFundo.clientWidth;
+        degrade2.style.width = `${imgFundoWidth}px`
+      };
+
+      transicao.addEventListener('transitionend', function () {
+        transicao.classList.remove('darken');
+
+        sinopse.innerHTML = carrocel[posicaoAtual].sinopse
+        imgLogo.src = carrocel[posicaoAtual].img
+        imgFundo.src = imagens[posicaoAtual]
+      }, { once: true }); // Ouvinte que será chamado apenas uma vez
+    }
   }
+
+
+  // function atualizarCarrossel() {
+  //   // Verifica se a posição atual é válida
+  //   if (posicaoAtual >= 0 && posicaoAtual < carrocel.length) {
+  //     imgFundo.onload = function () {
+  //       imgFundoWidth = imgFundo.clientWidth;
+  //       degrade2.style.width = `${imgFundoWidth}px`
+  //     };
+
+  //     sinopse.innerHTML = carrocel[posicaoAtual].sinopse
+  //     imgLogo.src = carrocel[posicaoAtual].img
+  //     imgFundo.src = imagens[posicaoAtual]
+  //   }
+  // }
+
+  function proximaImagem() {
+    posicaoAtual++;
+    if (posicaoAtual >= carrocel.length) {
+      posicaoAtual = 0; // Volta para a primeira imagem se atingir o final
+    }
+    atualizarCarrossel();
+  }
+
+  // Configura um intervalo para trocar automaticamente as imagens a cada 5 segundos
+  setInterval(proximaImagem, 4000);
+
+  // carrocel.addEventListener('click', proximaImagem);
 }
 
-function proximaImagem() {
-  posicaoAtual++;
-  if (posicaoAtual >= imagens.length) {
-    posicaoAtual = 0; // Volta para a primeira imagem se atingir o final
-  }
-  atualizarCarrossel();
-}
-
-// Configura um intervalo para trocar automaticamente as imagens a cada 5 segundos
-setInterval(proximaImagem, 4000);
-
-// carrocel.addEventListener('click', proximaImagem);
+main()
