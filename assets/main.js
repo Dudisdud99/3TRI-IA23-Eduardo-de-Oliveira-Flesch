f7049fc58b063f99b8e8a581ca6f3af458c203ec
Dudisdu99 <131018122+Dudisdud99@users.noreply.github.com>
2023-11-17 00:25:45 +0000

    arrumei varias funcionalidades do js

--- assets/main.js
+++ assets/main.js
@@ -12,6 +12,7 @@ button.addEventListener('click', () => {
 
 const caixas = document.querySelectorAll('.caixa');
 const dialog = document.querySelector("dialog")
+const fechar = document.querySelector(".fechar")
 
 caixas.forEach(caixa => {
   caixa.addEventListener('click', () => {
@@ -21,79 +22,57 @@ caixas.forEach(caixa => {
 });
 
 dialog.addEventListener('close', () => {
-  document.body.style.overflow = ''; 
+  document.body.style.overflow = '';
 });
+
+fechar.addEventListener("click", () => dialog.close())
+
 // --------------------------------------------
 
 const imgFundo = document.querySelector('.imgFundo');
 const degrade2 = document.querySelector('.degrade2');
 const imgLogo = document.querySelector('.imgLogo>img');
 const sinopse = document.querySelector('.carrocel p');
+const carrocelThumb = document.querySelector('.carrocelThumb');
 
+const duracaoIntervalo = 4000
+
+let posicaoAtual = 0;
+let intervalIdCarrossel;
 
 async function main() {
+  const request = await fetch("assets/carrocel.json");
+  const carrocel = await request.json();
+  const indicadores = document.querySelectorAll('.indicador');
 
-  const request = await fetch("assets/carrocel.json")
-  const carrocel = await request.json()
+  async function preCarregarImagens() {
+    const imagensPreCarregadas = [];
+
+    for (let i = 0; i < carrocel.length; i++) {
+      const img = new Image();
+      img.src = carrocel[i].imgAnime;
+      const imgLogo = new Image();
+      imgLogo.src = carrocel[i].imgLogo;
+
+      await Promise.all([img.decode(), imgLogo.decode()]);
+
+      imagensPreCarregadas.push({ img, imgLogo });
+    }
+
+    return imagensPreCarregadas;
+  }
+
+  const imagensPreCarregadas = await preCarregarImagens();
 
   var imgFundoWidth = imgFundo.clientWidth;
   degrade2.style.width = `${imgFundoWidth}px`;
 
-  let posicaoAtual = 0;
+  imgFundo.onload = function () {
+    imgFundoWidth = imgFundo.clientWidth;
+    degrade2.style.width = `${imgFundoWidth}px`
+  };
 
-  function atualizarCarrossel() {
-    const transicao = document.querySelector('.transicao');
-    transicao.classList.add('darken');
-
-    if (posicaoAtual >= 0 && posicaoAtual < carrocel.length) {
-      imgFundo.onload = function () {
-        imgFundoWidth = imgFundo.clientWidth;
-        degrade2.style.width = `${imgFundoWidth}px`
-      };
-
-      transicao.addEventListener('transitionend', function () {
-        transicao.classList.remove('darken');
-
-        sinopse.innerHTML = carrocel[posicaoAtual].sinopse
-        imgLogo.src = carrocel[posicaoAtual].imgLogo
-        imgFundo.src = carrocel[posicaoAtual].imgAnime
-      }, { once: true }); // Ouvinte que será chamado apenas uma vez
-    }
-  }
-
-  // function atualizarCarrossel() {
-  //   // Verifica se a posição atual é válida
-  //   if (posicaoAtual >= 0 && posicaoAtual < carrocel.length) {
-  //     imgFundo.onload = function () {
-  //       imgFundoWidth = imgFundo.clientWidth;
-  //       degrade2.style.width = `${imgFundoWidth}px`
-  //     };
-
-  //     sinopse.innerHTML = carrocel[posicaoAtual].sinopse
-  //     imgLogo.src = carrocel[posicaoAtual].img
-  //     imgFundo.src = imagens[posicaoAtual]
-  //   }
-  // }
-
-  function proximaImagem() {
-    posicaoAtual++;
-    if (posicaoAtual >= carrocel.length) {
-      posicaoAtual = 0;
-    }
-    atualizarCarrossel();
-  }
-  setInterval(proximaImagem, 4000);
-
-
-
-  const indicadores = document.querySelectorAll('.indicador');
-
-  function atualizarIndicadores() {
-    indicadores.forEach(indicador => indicador.classList.remove('ativo'));
-    indicadores[posicaoAtual].classList.add('ativo');
-  }
-
-  function proximaImagem() {
+  function proximaImagemCarrossel() {
     posicaoAtual++;
     if (posicaoAtual >= carrocel.length) {
       posicaoAtual = 0;
@@ -102,52 +81,63 @@ async function main() {
     atualizarIndicadores();
   }
 
+  function iniciarIntervaloCarrossel() {
+    intervalIdCarrossel = setInterval(proximaImagemCarrossel, duracaoIntervalo);
+  }
+
+  function atualizarIndicadores() {
+    indicadores.forEach(indicador => indicador.classList.remove('ativo'));
+    indicadores[posicaoAtual].classList.add('ativo');
+  }
+
+  function atualizarCarrossel() {
+    const transicao = document.querySelector('.transicao');
+    transicao.classList.add('darken');
+
+    if (posicaoAtual >= 0 && posicaoAtual < carrocel.length) {
+      transicao.addEventListener('transitionend', function () {
+        transicao.classList.remove('darken');
+        sinopse.innerHTML = carrocel[posicaoAtual].sinopse;
+        imgLogo.src = carrocel[posicaoAtual].imgLogo;
+        imgFundo.src = carrocel[posicaoAtual].imgAnime;
+      }, { once: true });
+    }
+  }
+
+
   indicadores.forEach((indicador, index) => {
     indicador.addEventListener('click', () => {
       posicaoAtual = index;
+      clearInterval(intervalIdCarrossel);
+      iniciarIntervaloCarrossel();
       atualizarCarrossel();
       atualizarIndicadores();
     });
   });
 
+  iniciarIntervaloCarrossel();
   atualizarIndicadores();
-
-
-
-  const carrocelThumb = document.querySelector('.carrocelThumb');
-
-  window.moverCarrossel = function (direcao) {
-    const caixaWidth = document.querySelector('.caixa').offsetWidth;
-    const espacoEntreCaixas = 15;
-    const elementosParaMover = 3;
-
-    const scrollAmount = (caixaWidth + espacoEntreCaixas) * elementosParaMover;
-    const currentPosition = carrocelThumb.scrollLeft;
-    let newPosition;
-
-    if (direcao === 1) {
-      // Rolar para a direita
-      newPosition = currentPosition + scrollAmount;
-    } else {
-      // Rolar para a esquerda
-      newPosition = currentPosition - scrollAmount;
-    }
-
-    carrocelThumb.scrollTo({
-      left: newPosition,
-      behavior: 'smooth'
-    });
-  }
-
-
-
-  // if (distanciaInicial < elementosVisiveis) {
-  //   caixa.style.filter = 'brightness(1)';
-  // } else {
-  //   caixa.style.filter = 'brightness(0.2)';
-  // }
-
-  console.log(carrocel.length)
 }
 
-main()
+main();
+
+function moverCarrocel(direcao) {
+  const caixaWidth = document.querySelector('.caixa').offsetWidth;
+  const espacoEntreCaixas = 15;
+  const elementosParaMover = 3;
+
+  const scrollAmount = (caixaWidth + espacoEntreCaixas) * elementosParaMover;
+  const currentPosition = carrocelThumb.scrollLeft;
+  let newPosition;
+
+  if (direcao === 1) {
+    newPosition = currentPosition + scrollAmount;
+  } else {
+    newPosition = currentPosition - scrollAmount;
+  }
+
+  carrocelThumb.scrollTo({
+    left: newPosition,
+    behavior: 'smooth'
+  });
+}
